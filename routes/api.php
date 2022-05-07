a30004b5c42db61761b3efc886202239f20d60a2
Pabeloszh <Pabeloszh@gmail.com>
2022-05-07 20:30:41 +0200

    added: genre

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VinylController;
+use App\Http\Controllers\GenreController;
 
 Route::get('/vinyls', [VinylController::class, 'index']);
 Route::post('/vinyls', [VinylController::class, 'store']);
@@ -10,6 +11,8 @@ Route::post('/vinyls', [VinylController::class, 'store']);
 Route::get('/vinyls/{id}', [VinylController::class, 'show']);
 Route::put('/vinyls/{id}', [VinylController::class, 'update']);
 Route::delete('/vinyls/{id}', [VinylController::class, 'destroy']);
+
+Route::get('/genres', [GenreController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
